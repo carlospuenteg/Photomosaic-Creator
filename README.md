49400871e4f080d297120823e96c5790e66d7b06
Carlos Puente <carlospuenteg@gmail.com>
2022-06-03 15:06:49 +0200

    Add files via upload

---
+++ README.md
@@ -0,0 +1,88 @@
+# Photomosaic Creator
+
+## Index
+- [Index](#index)
+- [Introduction](#introduction)
+  - [Main function (create_img)](#main-function-create_img)
+    - [Arguments (create_img)](#arguments-create_img)
+    - [Examples (create_img)](#examples-create_img)
+- [Possible errors](#possible-errors)
+
+## Introduction
+This program allows you to create a photomosaic from a set of images.
+
+The image and the set of images can be any size you want, but the more size, the more time it will take and the larger the resulting image will be.
+
+You can use the preset sets/folders of images or you can upload your own folders to the `images` folder.
+
+The more images the set has, the better the result, but the more time it will take to create the photomosaic.
+
+## Results
+
+<img src="./Examples/result_1/full.png" alt="example_img_full" width="500">
+<img src="./Examples/result_1/zoom_1.png" alt="example_img_zoom_1" width="500">
+<img src="./Examples/result_1/zoom_2.png" alt="example_img_zoom_2" width="500">
+<img src="./Examples/result_1/zoom_3.png" alt="example_img_zoom_4" width="500">
+
+## Run the script
+
+```bash
+$ python3 main.py
+```
+
+## How it works
+
+1. Creates the needed folders
+2. If `resize=True`, the `resized` folder will be replaced with the new resized images from the given set of images
+3. Obtains the average value of each primary color of each image in the `resized` folder
+4. Creates an array with all the numpy arrays of the images in the `resized` folder, converted from BGR to RGB
+5. Creates a numpy array from the selected main image in the `main-images` folder
+6. Creates the photomosaic by choosing the images with the closest average color for each pixel in the main image
+7. Saves the photomosaic in the `output` folder
+
+### Main function (create_img)
+
+#### Arguments (create_img)
+| argument | description | example | default value | Required |
+| -------- | ----------- | ------- | ------------- | -------- |
+| 1 | Name of the main image | "my_img.jpeg" | | Yes |
+| 2 | Size in px of the images that make up the main image | 50 | | Yes |
+| resize | Whether or not you want to resize the images | resize=True | False | No |
+| images_folder | Folder where the images are | images_folder="animals" | "animals" | No |
+| new_name | Name of the new image | new_name="photomosaic.jpg" | "photomosaic.jpg" | No |
+
+#### Examples (create_img)
+```python
+create_img( 
+  "img1.jpeg", 
+  100
+)
+create_img( 
+  "img1_high-res.jpeg",
+  50
+)
+create_img( 
+  "other_image.png", 
+  75, 
+  images_folder="landscapes"
+)
+create_img( 
+    "other_image.jpg", 
+    50, 
+    resize=True, 
+    images_folder="other_folder", 
+    new_name="my_new_photomosaic.png"
+)
+create_img( 
+    "other_image.jpeg", 
+    50, 
+    new_name="my_new_photomosaic.jpeg", 
+    resize=True
+)
+```
+
+## Possible errors
+
+If you get the error: `zsh: killed python3 main.py`, it means that the program is taking too long to run (it's taking too much memory).
+
+To solve it, you can try to reduce the size of the resized images or of the main image.
--- main.py
+++ main.py
@@ -10,26 +10,50 @@ Images taken from:
 """
 
 #------------------------------------------------------------------------------
+# Constants
 
-def get_colors(folder="resized"):
+RESIZED_FOLDER = "resized"
+IMAGES_FOLDER = "images"
+OUTPUT_FOLDER = "output"
+MAIN_IMAGES_FOLDER = "main-images"
+
+#------------------------------------------------------------------------------
+# ANSI COLORS for the terminal
+
+class Ansi:
+    BLACK = '\u001b[30m'
+    RED = '\u001b[31m'
+    GREEN = '\u001b[32m'
+    YELLOW = '\u001b[33m'
+    BLUE = '\u001b[34m'
+    MAGENTA = '\u001b[35m'
+    CYAN = '\u001b[36m'
+    WHITE = '\u001b[37m'
+    RESET = '\u001b[0m'
+
+#------------------------------------------------------------------------------
+# Gets the average value of each primary color of each image in the resized images folder
+
+def get_colors():
     res = []
-    folders = [str(i) + ".jpg" for i in range(len(os.listdir(folder)))]
+    folders = [str(i) + ".jpg" for i in range(len(os.listdir(RESIZED_FOLDER)))]
+
     for file in folders:
-        print("Analyzing " + file)
-        if not file.startswith('.'):
-            avg = [0,0,0]
-            img = np.array(Image.open(folder + "/" + file))
-            for line in img:
-                for pixel in line:
-                    avg[0] += pixel[0]
-                    avg[1] += pixel[1]
-                    avg[2] += pixel[2]
-            avg = [ int( c / (len(img) * len(img[0])) ) for c in avg]
-            res.append(avg)
+        print(f"{Ansi.CYAN}Analyzing{Ansi.RESET} {file}")
+        avg = [0,0,0]
+        img = np.array(Image.open(RESIZED_FOLDER + "/" + file))
+        for line in img:
+            for pixel in line:
+                avg[0] += pixel[0]
+                avg[1] += pixel[1]
+                avg[2] += pixel[2]
+        avg = [ int( color/(len(img) * len(img[0])) ) for color in avg]
+        res.append(avg)
 
     return res
 
 #------------------------------------------------------------------------------
+# Gets the index of the image with the most similar colors to the given pixel
 
 def closest(arr, color):
     index = 0
@@ -42,25 +66,58 @@ def closest(arr, color):
     return index
 
 #------------------------------------------------------------------------------
+# Resizes each image from the given folder to the given size
 
-def main(file="img_test.jpeg", size=10, format=True, folder="animals"):
-    if format: 
-        for f in os.listdir("resized"):
-            os.remove("resized/" + f)
-        os.system("python3 resize.py " + str(size) + " " + str(size) + " " + folder)
+def resize_images(size, folder=IMAGES_FOLDER + "/animals"):
+    i = 0
+    for file in os.listdir(folder):
+        if not file.startswith('.'):
+            print(f"{Ansi.GREEN}Resizing{Ansi.RESET} {file}")
+            img = Image.open(folder + "/" + file)
+            resized_img = img.resize((size, size))
+            resized_img.save(RESIZED_FOLDER + "/" + str(i) + ".jpg")
+            i += 1
+
+#------------------------------------------------------------------------------
+# Creates the needed folders
+
+def create_folders():
+    if not os.path.exists(RESIZED_FOLDER):
+        os.mkdir(RESIZED_FOLDER)
+    if not os.path.exists(OUTPUT_FOLDER):
+        os.makedirs(OUTPUT_FOLDER)
+    if not os.path.exists(MAIN_IMAGES_FOLDER):
+        os.makedirs(MAIN_IMAGES_FOLDER)
+
+#------------------------------------------------------------------------------
+# This is executed when the script is run
+
+def create_img(main_image, images_size, **args): # resize=True, images_folder="animals"
+    resize = args.get("resize", False)
+    images_folder = args.get("images_folder", "animals")
+    new_name = args.get('new_name', "photomosaic.jpg")
+
+    create_folders()
+
+    if resize: 
+        for f in os.listdir(RESIZED_FOLDER):
+            os.remove(RESIZED_FOLDER + "/" + f)
+        resize_images(images_size, IMAGES_FOLDER + "/" + images_folder)
 
     images_avg = get_colors()
-    images = [ np.array(Image.open("resized/" + str(i) + ".jpg"))[:,:,::-1] for i in range(len(images_avg)) ]
-    main_img = np.array(Image.open("main-image/" + file))
-    new_img = np.zeros((len(main_img)*size, len(main_img[0])*size, 3), dtype=np.uint8)
+    images = [ np.array(Image.open(RESIZED_FOLDER + "/" + str(i) + ".jpg"))[:,:,::-1] for i in range(len(images_avg)) ] # [:,:,::-1] to convert from BGR to RGB
+    main_img = np.array(Image.open(MAIN_IMAGES_FOLDER + "/" + main_image))
+    new_img = np.zeros((len(main_img)*images_size, len(main_img[0])*images_size, 3), dtype=np.uint8)
 
     for i,line in enumerate(main_img):
-        print("Creating... " + str(round(i/len(main_img)*100,2)) + "%")
+        print(f"{Ansi.MAGENTA}Creating...{Ansi.RESET} {round(i/len(main_img)*100,2)}%")
         for j,pixel in enumerate(line):
-            new_img[i*size:i*size+size,j*size:j*size+size] = images[closest(images_avg, pixel)]
+            s = images_size
+            new_img[i*s : i*s+s , j*s : j*s+s] = images[closest(images_avg, pixel)]
 
-    cv2.imwrite("result/" + file, new_img)
+    print(f"{Ansi.YELLOW}Saving...{Ansi.RESET}")
+    cv2.imwrite(OUTPUT_FOLDER + "/" + new_name, new_img)
 
 #------------------------------------------------------------------------------
 
-main("img3-higher.jpeg", 50 , True, "animals")
+create_img( "img1.jpeg" , 50 , resize=True)
